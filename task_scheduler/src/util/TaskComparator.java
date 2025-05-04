25126e55e18ee4e6269c48430f973f97a427f042
E Wilber <157968704+e-wilber@users.noreply.github.com>
2025-05-04 01:09:12 -0500

    Update TaskComparator.java

--- task_scheduler/src/util/TaskComparator.java
+++ task_scheduler/src/util/TaskComparator.java
@@ -1,3 +1,20 @@
+/** 
+* DATA STRUCTURES: FINAL PROJECT
+* @author E Wilber
+* @version 1.0
+* @since 1.0
+*  
+* OS: Windows11
+* IDE: Eclipse
+*  
+* Copyright : This is my own original work 
+* based on specifications issued by our instructor
+* Academic Honesty: I attest that this is my original work.
+* I have not used unauthorized source code, either modified or
+* unmodified, nor used generative AI as a final draft. 
+* I have not given other fellow student(s) access to my program.
+*/
+
 //This is used both by the PriorityQueue (TaskScheduler.java) and by custom heap sort (HeapSort.java)
 package util;
 import model.Task;
