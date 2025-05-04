76c63e0901b36dedb0e903586a817ec1c545a192
E Wilber <157968704+e-wilber@users.noreply.github.com>
2025-05-04 01:07:32 -0500

    Complete TaskScheduler.java
    added header

--- task_scheduler/src/scheduler/TaskScheduler.java
+++ task_scheduler/src/scheduler/TaskScheduler.java
@@ -1,3 +1,20 @@
+/** 
+* DATA STRUCTURES: FINAL PROJECT
+* @author E Wilber
+* @version 1.0
+* @since 1.0
+*  
+* OS: Windows11
+* IDE: Eclipse
+*  
+* Copyright : This is my own original work 
+* based on specifications issued by our instructor
+* Academic Honesty: I attest that this is my original work.
+* I have not used unauthorized source code, either modified or
+* unmodified, nor used generative AI as a final draft. 
+* I have not given other fellow student(s) access to my program.
+*/
+
 //BINARY HEAP DATA STRUCTURE
 package scheduler;
 import model.Task;
